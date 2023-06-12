12893ddb527a06cfcbedb555c8b235575657f6bb
massa <zoumaniguimassa12@gmail.com>
2023-06-12 15:51:53 +0000

    animals database

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 /* Populate database with sample data. */
-
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (1, 'Agumon', '2020-02-03', 0, true, 10.23);
+ INSERT INTO animals VALUES (2, 'Gabumon', '2028-11-15', 2, true,8);
+INSERT INTO animals VALUES (3, 'Pikachu', '2021-01-07',1,false,15.04);
+ INSERT INTO animals VALUES (4, 'Devimon', '2017-03-12',5,false,11);
