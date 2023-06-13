bd3b3656eb925553b48b237c90d1ffe5c9cdcef5
massa <zoumaniguimassa12@gmail.com>
2023-06-13 14:46:23 +0000

    id removed column from the INSERT statements

--- data.sql
+++ data.sql
@@ -1,34 +1,34 @@
 /* Populate database with sample data. */
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES (1, 'Agumon', '2020-02-03', 0, true, 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Agumon', '2020-02-03', 0, true, 10.23);
 
-INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES (2, 'Gabumon', '2028-11-15', 2, true,8);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Gabumon', '2028-11-15', 2, true,8);
 
-INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES (3, 'Pikachu', '2021-01-07',1,false,15.04);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Pikachu', '2021-01-07',1,false,15.04);
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES (4, 'Devimon', '2017-03-12',5,false,11);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Devimon', '2017-03-12',5,false,11);
 
-INSERT INTO animals ( id,name, date_of_birth, escape_attempts, neutered, weight_kg, species) 
-VALUES (5, 'Charmander','2020-02-08',0,false,-11,'unknown');
-
-INSERT INTO animals 
-VALUES (6, 'Plantmon', '2021-11-15', 2, true, -5.7, 'unknown');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species) 
+VALUES ('Charmander','2020-02-08',0,false,-11,'unknown');
 
 INSERT INTO animals 
-VALUES (7, 'Squirtle', '1993-04-02', 3, false, -12.13, 'unknown');
+VALUES ('Plantmon', '2021-11-15', 2, true, -5.7, 'unknown');
 
 INSERT INTO animals 
-VALUES (8, 'Angemon', '2005-06-12', 1, true, -45, 'unknown');
+VALUES ('Squirtle', '1993-04-02', 3, false, -12.13, 'unknown');
 
 INSERT INTO animals 
-VALUES (9,'Boarmon', '2005-06-07', 7, true, 20.4, 'unknown');
+VALUES ('Angemon', '2005-06-12', 1, true, -45, 'unknown');
 
 INSERT INTO animals 
-VALUES (10,'Blossom', '1998-10-13', 3, true, 17, 'unknown');
+VALUES ('Boarmon', '2005-06-07', 7, true, 20.4, 'unknown');
 
 INSERT INTO animals 
-VALUES (11,'Ditto', '2022-05-14', 4, true, 22, 'unknown');
+VALUES ('Blossom', '1998-10-13', 3, true, 17, 'unknown');
+
+INSERT INTO animals 
+VALUES ('Ditto', '2022-05-14', 4, true, 22, 'unknown');
 
