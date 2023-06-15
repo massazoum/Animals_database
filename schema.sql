b85da36fd53a2373367031f2b1b1c0093630e71b
massa <zoumaniguimassa12@gmail.com>
2023-06-15 10:33:28 +0000

    Update

--- schema.sql
+++ schema.sql
@@ -6,7 +6,23 @@
     escape_attempts INTEGER,
     neutered BOOLEAN,
     weight_kg DECIMAL(10,2));
-
-    
+  
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(255);
+
+CREATE TABLE owners (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INT
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+ALTER TABLE animals
+ADD COLUMN species_id INT REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT REFERENCES owners(id);
